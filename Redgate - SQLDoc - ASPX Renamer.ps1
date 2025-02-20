6823cdab65f221744c64b581e2fe86ebf730d848
Jon Kirkwood <re_kirky@hotmail.com>
2025-02-21 09:51:21 +1000

    SQL Doc - ASPX Renamer
    Was doing this for a ticket where the user wanted to host SQL Doc files on Sharepoint.
    
    Created a script that renames all references of HTML to ASPX so it could load natively in a Sharepoint environment
    
    Set the path where your exported SQL Doc files are and then it recursively renames files and references within files

---
+++ Redgate - SQLDoc - ASPX Renamer.ps1
@@ -0,0 +1,15 @@
+﻿$folderPath = "C:\Temp\(local)__documentation"
+
+#Get all .html files recursively
+Get-ChildItem -Path $folderPath -Filter "*.html" -Recurse | ForEach-Object {
+    $newName = $_.FullName -replace "\.html$", ".aspx"
+
+    #Read file content, replace "html" with "aspx", and save
+    (Get-Content $_.FullName) -replace "html", "aspx" | Set-Content $newName
+
+    # Display modified file name - used for debugging, remove if process running smoothly
+    Write-Host "Modified: $($_.FullName) -> $newName"
+
+    # Remove the original .html file
+    Remove-Item $_.FullName
+}
